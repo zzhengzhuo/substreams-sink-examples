aac848fefea8950ce0a46b87183a20e7ec3afe2d
zz <48124374+zzhengzhuo@users.noreply.github.com>
2024-12-12 22:40:12 +0800

    feat: test latest substreams package

--- rust/src/pb/pb.rs
+++ rust/src/pb/pb.rs
@@ -1,10 +1,19 @@
 // @generated
+pub mod mydata {
+    // @@protoc_insertion_point(attribute:mydata.v1)
+    pub mod v1 {
+        include!("mydata.v1.rs");
+        // @@protoc_insertion_point(mydata.v1)
+    }
+}
 pub mod sf {
-    pub mod firehose {
-        // @@protoc_insertion_point(attribute:sf.firehose.v2)
-        pub mod v2 {
-            include!("sf.firehose.v2.rs");
-            // @@protoc_insertion_point(sf.firehose.v2)
+    pub mod solana {
+        pub mod r#type {
+            // @@protoc_insertion_point(attribute:sf.solana.type.v1)
+            pub mod v1 {
+                include!("sf.solana.type.v1.rs");
+                // @@protoc_insertion_point(sf.solana.type.v1)
+            }
         }
     }
     // @@protoc_insertion_point(attribute:sf.substreams)
@@ -16,13 +25,6 @@ pub mod sf {
             pub mod v1 {
                 include!("sf.substreams.index.v1.rs");
                 // @@protoc_insertion_point(sf.substreams.index.v1)
-            }
-        }
-        pub mod internal {
-            // @@protoc_insertion_point(attribute:sf.substreams.internal.v2)
-            pub mod v2 {
-                include!("sf.substreams.internal.v2.rs");
-                // @@protoc_insertion_point(sf.substreams.internal.v2)
             }
         }
         pub mod rpc {
@@ -41,10 +43,33 @@ pub mod sf {
                 }
             }
         }
+        pub mod solana {
+            // @@protoc_insertion_point(attribute:sf.substreams.solana.v1)
+            pub mod v1 {
+                include!("sf.substreams.solana.v1.rs");
+                // @@protoc_insertion_point(sf.substreams.solana.v1)
+            }
+        }
         // @@protoc_insertion_point(attribute:sf.substreams.v1)
         pub mod v1 {
             include!("sf.substreams.v1.rs");
             // @@protoc_insertion_point(sf.substreams.v1)
+        }
+    }
+}
+pub mod sol {
+    pub mod instructions {
+        // @@protoc_insertion_point(attribute:sol.instructions.v1)
+        pub mod v1 {
+            include!("sol.instructions.v1.rs");
+            // @@protoc_insertion_point(sol.instructions.v1)
+        }
+    }
+    pub mod transactions {
+        // @@protoc_insertion_point(attribute:sol.transactions.v1)
+        pub mod v1 {
+            include!("sol.transactions.v1.rs");
+            // @@protoc_insertion_point(sol.transactions.v1)
         }
     }
 }
